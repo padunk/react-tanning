3ad9d2ea91bae6f943c9c878589ac09b99ffc481
padunk <abraham.agel@gmail.com>
2025-02-22 12:51:45 +0700

    better query example

--- src/routes/query.lazy.tsx
+++ src/routes/query.lazy.tsx
@@ -6,10 +6,12 @@ export const Route = createLazyFileRoute('/query')({
 })
 
 function Query() {
-  const { data, status } = useQuery({
+  const { data, status } = useQuery<{ message: Array<string> }>({
     queryKey: ['fetchDogs'],
     queryFn: async () => {
-      const res = await fetch('https://dog.ceo/api/breeds/image/random')
+      const res = await fetch(
+        'https://dog.ceo/api/breed/terrier/american/images'
+      )
       return await res.json()
     },
   })
@@ -17,8 +19,18 @@ function Query() {
     <div className='p-2'>
       <p>Watch out for Dogs...</p>
       {status === 'success' ? (
-        <div className='overflow-hidden w-[640px] h-[480px]'>
-          <img src={data.message} alt='dog' className='w-full' />
+        <div className='grid grid-cols-4 gap-2'>
+          {data.message.map((src: string) => {
+            return (
+              <div className='overflow-hidden h-64'>
+                <img
+                  src={src}
+                  alt='dog'
+                  className='w-full h-full object-cover'
+                />
+              </div>
+            )
+          })}
         </div>
       ) : (
         <p>Working...</p>
